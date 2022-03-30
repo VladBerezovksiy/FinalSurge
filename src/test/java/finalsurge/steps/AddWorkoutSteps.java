60d5546756acf12b852f37fa00e21d5bfe9ba9e6
VladBerezovksiy <vladberezovksiy@gmail.com>
2022-03-30 15:05:44 +0300

    **

--- src/test/java/finalsurge/steps/AddWorkoutSteps.java
+++ src/test/java/finalsurge/steps/AddWorkoutSteps.java
@@ -1,10 +1,17 @@
 package finalsurge.steps;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 
 public class AddWorkoutSteps extends AbstractSteps {
 
     public AddWorkoutSteps(WebDriver driver) {
         super(driver);
+    }
+
+    @Step("Create new Workout")
+    public AddWorkoutSteps createNewWorkout() {
+
+        return this;
     }
 }
