bd9ca14747d9cf2fab66ef413ca83ff761f094e6
VladBerezovksiy <vladberezovksiy@gmail.com>
2022-04-07 16:50:23 +0300

    Add retryAnalyzer into AddWorkoutTest

--- src/test/java/finalsurge/tests/positive/AddWorkoutTest.java
+++ src/test/java/finalsurge/tests/positive/AddWorkoutTest.java
@@ -1,12 +1,13 @@
 package finalsurge.tests.positive;
 
 import finalsurge.tests.BaseTest;
+import finalsurge.utils.Retry;
 import models.WorkoutModel;
 import org.testng.annotations.Test;
 
 public class AddWorkoutTest extends BaseTest {
 
-    @Test (groups="smoke")
+    @Test (groups="smoke", retryAnalyzer = Retry.class)
     public void addWorkoutTest() {
         mainSteps
                 .openLoginPage()
