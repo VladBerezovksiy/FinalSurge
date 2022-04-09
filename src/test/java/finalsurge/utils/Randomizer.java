6858bd2baa97ce56b4efacc74d7a2c58175c5357
VladBerezovksiy <vladberezovksiy@gmail.com>
2022-04-09 19:18:09 +0300

    Change Randomizer

--- src/test/java/finalsurge/utils/Randomizer.java
+++ src/test/java/finalsurge/utils/Randomizer.java
@@ -1,12 +1,10 @@
 package finalsurge.utils;
 
-import org.bouncycastle.asn1.cms.TimeStampedData;
-
-
 import java.sql.Timestamp;
-import java.util.Date;
 
 public class Randomizer {
-    public static    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-public static long timeData = timestamp.getTime();
+
+    public static Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+    public static long timeData = timestamp.getTime();
+
 }
